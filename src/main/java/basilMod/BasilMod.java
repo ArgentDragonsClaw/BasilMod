ee9ce204be6bd8a922daaacef92f9dbaaf377c60
Evie Diver <e.phazon@gmail.com>
2019-10-21 09:33:13 -0500

    Added Counterstatus

--- src/main/java/basilMod/BasilMod.java
+++ src/main/java/basilMod/BasilMod.java
@@ -433,6 +433,7 @@ public class BasilMod implements
         library.add(new CaffeineJitters());
         library.add(new SleeplessNight());
         library.add(new Kaluha());
+        library.add(new Counterstatus());
 
         int counter_basic = 0;
         int counter_common = 0;
---
+++ src/main/java/basilMod/cards/powers/Counterstatus.java
@@ -0,0 +1,67 @@
+package basilMod.cards.powers;
+
+import basilMod.cards.AbstractDynamicCard;
+import basilMod.BasilMod;
+import basilMod.characters.TheScholar;
+
+import basilMod.powers.CounterstatusPower;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static basilMod.BasilMod.makeCardPath;
+
+public class Counterstatus extends AbstractDynamicCard {
+
+
+// TEXT DECLARATION
+
+    public static final String ID = BasilMod.makeID(Counterstatus.class.getSimpleName()); // USE THIS ONE FOR THE TEMPLATE;
+    public static final String IMG = makeCardPath("Counterstatus.png");// "public static final String IMG = makeCardPath("Counterstatus.png");
+
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+
+// /TEXT DECLARATION/
+
+
+// STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = TheScholar.Enums.BASIL_PURPLE;
+
+    private static final int COST = 2;
+    private static final int MAGIC = 5;
+
+// /STAT DECLARATION/
+
+
+    public Counterstatus() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = magicNumber = MAGIC;
+
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new CounterstatusPower(p, p, magicNumber, upgraded)));
+    }
+
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
