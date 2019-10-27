985cee4db9a3f8227e2eef5dc3cebe66ffd26ac5
Evie Diver <e.phazon@gmail.com>
2019-10-27 15:23:05 -0500

    Trick Coin now resets its power tip on equip, and has no cap on flipping.

--- src/main/java/basilMod/relics/TrickCoin.java
+++ src/main/java/basilMod/relics/TrickCoin.java
@@ -32,7 +32,6 @@ public class TrickCoin extends CustomRelic implements ClickableRelic { // You mu
     private static final Texture IMG_TAILS = TextureLoader.getTexture(makeRelicPath("trick_coin_tails.png"));
     private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("default_clickable_relic.png")); // im lazy and theyre both circles
 
-    private boolean usedThisTurn = false; // You can also have a relic be only usable once per combat. Check out Hubris for more examples, including other StSlib things.
     private boolean isPlayerTurn = false; // We should make sure the relic is only activateable during our turn, not the enemies'.
 
     public boolean isHeads = true;
@@ -47,17 +46,19 @@ public class TrickCoin extends CustomRelic implements ClickableRelic { // You mu
     @Override
     public void onEquip() {
         this.description = getUpdatedDescription();
+        tips.clear();
+        tips.add(new PowerTip(name + " (Heads)", description));
     }
 
     @Override
     public void onRightClick() {// On right click
-        if (!isObtained || usedThisTurn || !isPlayerTurn) {
+        if (!isObtained || !isPlayerTurn) {
             // If it has been used this turn, the player doesn't actually have the relic (i.e. it's on display in the shop room), or it's the enemy's turn
             return; // Don't do anything.
         }
 
         if (AbstractDungeon.getCurrRoom() != null && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) { // Only if you're in combat
-            usedThisTurn = true; // Set relic as "Used this turn"
+
             flash(); // Flash
             stopPulse(); // And stop the pulsing animation (which is started in atPreBattle() below)
 
@@ -83,12 +84,10 @@ public class TrickCoin extends CustomRelic implements ClickableRelic { // You mu
 
     @Override
     public void atPreBattle() {
-        usedThisTurn = false; // Make sure usedThisTurn is set to false at the start of each combat.
         beginLongPulse();     // Pulse while the player can click on it.
     }
 
     public void atTurnStart() {
-        usedThisTurn = false;  // Resets the used this turn. You can remove this to use a relic only once per combat rather than per turn.
         isPlayerTurn = true; // It's our turn!
         beginLongPulse(); // Pulse while the player can click on it.
     }
